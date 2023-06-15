45e43d6ba14792a2d65e9e09bacbcc23c723dfe5
Nitin <nitinrawat470@gmail.com>
2023-06-15 12:55:38 +0530

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createServer } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    before: (app, server) => {
+      app.use((req, res, next) => {
+        // Set security headers
+        res.setHeader('Content-Security-Policy', 'default-src');
+        res.setHeader('X-Content-Type-Options', 'nosniff');
+        // Add more security headers as needed
+
+        next();
+      });
+    },
+  },
+});
+
+
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
